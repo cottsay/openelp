cottsay/openelp

commit 5796c3dd0efd3114e0e2ec28ddf13850952b8078

Win32 fixes, win32 signal handling, debug flag

src/proxy.c
@@ -356,9 +356,13 @@ void proxy_shutdown(struct proxy_handle *ph)
 	{
 		struct proxy_priv *priv = (struct proxy_priv *)ph->priv;
 
-		conn_shutdown(&priv->conn_client);
+		proxy_log(ph, LOG_LEVEL_DEBUG, "Shutting down client connection...\n");
 
 		ph->status = PROXY_STATUS_SHUTDOWN;
+
+		conn_shutdown(&priv->conn_client);
+
+		proxy_log(ph, LOG_LEVEL_DEBUG, "Client connection has been shut down.\n");
 	}
 }
 
@@ -367,7 +371,7 @@ void proxy_log(struct proxy_handle *ph, enum LOG_LEVEL lvl, const char *fmt, ...
 	struct proxy_priv *priv = (struct proxy_priv *)ph->priv;
 	va_list args;
 
-	if (priv == NULL || lvl > priv->log.level)
+	if (priv == NULL || (unsigned)lvl > priv->log.level)
 	{
 		return;
 	}
@@ -430,7 +434,12 @@ int proxy_process(struct proxy_handle *ph)
 		ret = conn_recv(&priv->conn_client, priv->conn_client_buff, sizeof(struct proxy_msg));
 		if (ret < 0)
 		{
+#ifdef _WIN32
+			// Hack for not knowing if we shutdown the connection or the client did
+			if (ret != -EINTR && (ret != -EPIPE || ph->status != PROXY_STATUS_SHUTDOWN))
+#else
 			if (ret != -EINTR)
+#endif
 			{
 				proxy_handle_client_error(ph, ret);
 			}
@@ -929,6 +938,8 @@ void * tcp_forwarder(void *ctx)
 
 				conn_close(fc->conn);
 
+				proxy_log(fc->ph, LOG_LEVEL_DEBUG, "TCP thread is returning due to a client connection error\n");
+
 				return NULL;
 			}
 		}
@@ -945,8 +956,11 @@ void * tcp_forwarder(void *ctx)
 	if (ret < 0)
 	{
 		// This is an error with this connection
-		proxy_handle_client_error(fc->ph, ret);
+		proxy_log(fc->ph, LOG_LEVEL_WARN, "TCP communication error (%d). Disconnecting...\n", -ret);
+		conn_shutdown(&priv->conn_client);
 	}
+
+	proxy_log(fc->ph, LOG_LEVEL_DEBUG, "TCP thread is returning cleanly\n");
 
 	return NULL;
 }
@@ -984,6 +998,8 @@ void * udp_forwarder(void *ctx)
 
 				conn_close(fc->conn);
 
+				proxy_log(fc->ph, LOG_LEVEL_DEBUG, "UDP thread is returning due to a client connection error\n");
+
 				return NULL;
 			}
 		}
@@ -1002,14 +1018,17 @@ void * udp_forwarder(void *ctx)
 	switch (ret)
 	{
 	case -ECONNRESET:
+	case -EINTR:
 	case -ENOTCONN:
 	case -EPIPE:
 		break;
 	default:
-		proxy_log(fc->ph, LOG_LEVEL_WARN, "UDP communication error. Disconnecting...\n");
-		proxy_drop(fc->ph);
+		proxy_log(fc->ph, LOG_LEVEL_WARN, "UDP communication error (%d). Disconnecting...\n", -ret);
+		conn_shutdown(&priv->conn_client);
 		break;
 	}
+
+	proxy_log(fc->ph, LOG_LEVEL_DEBUG, "UDP thread is returning cleanly\n");
 
 	return NULL;
 }

src/conf.c
@@ -42,6 +42,62 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int conf_readline(char **lineptr, size_t *n, FILE *stream)
+{
+	size_t so_far = 0;
+	char temp;
+	char *temp_ptr;
+
+	if (*lineptr == NULL)
+	{
+		*lineptr = malloc(128);
+		if (*lineptr == NULL)
+		{
+			return -ENOMEM;
+		}
+
+		*n = 128;
+	}
+	else if (*n == 0)
+	{
+		temp_ptr = realloc(*lineptr, 128);
+		if (temp_ptr == NULL)
+		{
+			return -ENOMEM;
+		}
+
+		*lineptr = temp_ptr;
+		*n = 128;
+	}
+
+	while ((temp = fgetc(stream)) != EOF)
+	{
+		// Need more space?
+		if (so_far + 1 >= *n)
+		{
+			temp_ptr = realloc(*lineptr, *n + 128);
+			if (temp_ptr == NULL)
+			{
+				return -ENOMEM;
+			}
+
+			*lineptr = temp_ptr;
+			*n += 128;
+		}
+
+		(*lineptr)[so_far++] = temp;
+
+		if (temp == '\n')
+		{
+			break;
+		}
+	}
+
+	(*lineptr)[so_far] = '\0';
+
+	return so_far;
+}
+
 int conf_init(struct proxy_conf *conf)
 {
 	conf->password = NULL;
@@ -67,7 +123,7 @@ int conf_parse_line(const char *line, struct proxy_conf *conf)
 	size_t val_len = 0;
 
 	// Find the beginning of the key
-	for (; *key == ' ' && *key == '\t'; key++);
+	for (; *key == ' ' || *key == '\t' || *key == '\n' || *key == '\r'; key++);
 
 	// If the line is a comment or empty, ignore it
 	if (*key == '#' || *key == '\0' || *key == '=')
@@ -155,7 +211,7 @@ int conf_parse_stream(FILE *stream, struct proxy_conf *conf)
 	size_t line_len = 0;
 	int ret = 0;
 
-	while (getline(&line, &line_len, stream) > 0)
+	while (conf_readline(&line, &line_len, stream) > 0)
 	{
 		ret = conf_parse_line(line, conf);
 

src/conn.c
@@ -258,7 +258,12 @@ int conn_listen_wait(struct proxy_conn *pc)
 
 	if (priv->conn_fd == INVALID_SOCKET)
 	{
+#ifdef _WIN32
+		int ret = SOCK_ERRNO;
+		return ret == -WSAEINTR ? -EINTR : ret;
+#else
 		return SOCK_ERRNO;
+#endif
 	}
 
 	mutex_lock(&priv->mutex);
@@ -403,6 +408,10 @@ int conn_recv_any(struct proxy_conn *pc, uint8_t *buff, size_t buff_len, uint32_
 		if (ret == -WSAECONNRESET)
 		{
 			ret = -ECONNRESET;
+		}
+		else if (ret == -WSAEINTR)
+		{
+			ret = -EINTR;
 		}
 #endif
 	}
@@ -616,6 +625,11 @@ void conn_shutdown(struct proxy_conn *pc)
 	if (priv->sock_fd != INVALID_SOCKET)
 	{
 		shutdown(priv->sock_fd, SHUT_RDWR);
+#ifdef _WIN32
+		// Hack to cancel an in-progress accept
+		closesocket(priv->sock_fd);
+		priv->sock_fd = INVALID_SOCKET;
+#endif
 	}
 
 	mutex_unlock_shared(&priv->mutex);

src/log.c
@@ -168,7 +168,7 @@ void log_printf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, ...
 {
 	va_list args;
 
-	if (lvl > log->level)
+	if ((unsigned)lvl > log->level)
 	{
 		return;
 	}
@@ -256,7 +256,7 @@ void log_vprintf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, va
 {
 	struct log_priv *priv = (struct log_priv *)log->priv;
 
-	if (lvl > log->level)
+	if ((unsigned)lvl > log->level)
 	{
 		return;
 	}
@@ -279,12 +279,12 @@ void log_vprintf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, va
 	case LOG_MEDIUM_FILE:
 		{
 			time_t epoch;
-			struct tm cal_time;
+			struct tm *cal_time;
 			char tstamp[16];
 
 			time(&epoch);
-			localtime_r(&epoch, &cal_time);
-			strftime(tstamp, 16, "%b %d %H:%M:%S", &cal_time);
+			cal_time = localtime(&epoch);
+			strftime(tstamp, 16, "%b %d %H:%M:%S", cal_time);
 			fprintf(priv->medium_file.fp, "%s : ", tstamp);
 
 			vfprintf(priv->medium_file.fp, fmt, args);

src/proxyd.c
@@ -40,6 +40,8 @@
 #ifndef _WIN32
 #  include <signal.h>
 #  include <sys/stat.h>
+#else
+#  include <Windows.h>
 #endif
 
 #include <errno.h>
@@ -58,6 +60,7 @@ const char default_config_path[] = "ELProxy.conf";
 struct proxy_opts
 {
 	const char *config_path;
+	uint8_t debug;
 	uint8_t foreground;
 	const char *log_path;
 	uint8_t syslog;
@@ -78,6 +81,20 @@ void graceful_shutdown(int signum, siginfo_t *info, void *ptr)
 
 	proxy_shutdown(&ph);
 }
+#else
+BOOL WINAPI graceful_shutdown(DWORD dwCtrlType)
+{
+	if (dwCtrlType == CTRL_C_EVENT)
+	{
+		proxy_log(&ph, LOG_LEVEL_INFO, "Caught signal\n");
+
+		proxy_shutdown(&ph);
+
+		return TRUE;
+	}
+
+	return FALSE;
+}
 #endif
 
 /*
@@ -87,8 +104,9 @@ void print_usage(void)
 {
 #ifndef _WIN32
 	printf("OpenELP - Open EchoLink Proxy %d.%d.%d\n\n"
-		"Usage: openelpd [-F] [-L <log path>] [-S] [--help] [<config path>]\n\n"
+		"Usage: openelpd [-d] [-F] [-L <log path>] [-S] [--help] [<config path>]\n\n"
 		"Arguments:\n"
+		"    -d            Enable debugging output\n"
 		"    -F            Stay in foreground (don't daemonize)\n"
 		"    -L <log path> Log output the given log file\n"
 		"    -S            Use syslog for logging\n"
@@ -96,8 +114,9 @@ void print_usage(void)
 		OPENELP_MAJOR_VERSION, OPENELP_MINOR_VERSION, OPENELP_PATCH_VERSION);
 #else
 	printf("OpenELP - Open EchoLink Proxy %d.%d.%d\n\n"
-		"Usage: openelpd [-L <log path>] [--help] [<config path>]\n"
+		"Usage: openelpd [-d] [-L <log path>] [--help] [<config path>]\n"
 		"Arguments:\n"
+		"    -d            Enable debugging output\n"
 		"    -L <log path> Log output the given log file\n"
 		"    <config path> Path to the proxy configuration. Defaults to ELProxy.conf\n",
 		OPENELP_MAJOR_VERSION, OPENELP_MINOR_VERSION, OPENELP_PATCH_VERSION);
@@ -130,6 +149,9 @@ void parse_args(const int argc, const char *argv[], struct proxy_opts *opts)
 				{
 					switch (argv[i][j])
 					{
+					case 'd':
+						opts->debug = 1;
+						break;
 #ifndef _WIN32
 					case 'F':
 						opts->foreground = 1;
@@ -223,6 +245,11 @@ int main(int argc, const char *argv[])
 
 	sigaction(SIGINT, &sigact, NULL);
 	sigaction(SIGTERM, &sigact, NULL);
+#else
+	if (!SetConsoleCtrlHandler(graceful_shutdown, TRUE))
+	{
+		fprintf(stderr, "Failed to set SIGINT handler (%d)\n", GetLastError());
+	}
 #endif
 
 	// Initialize proxy
@@ -232,6 +259,9 @@ int main(int argc, const char *argv[])
 		fprintf(stderr, "Failed to initialize proxy (%d): %s\n", -ret, strerror(-ret));
 		exit(ret);
 	}
+
+	// Set the logging level
+	proxy_log_level(&ph, opts.debug ? LOG_LEVEL_DEBUG : LOG_LEVEL_INFO);
 
 	// Load the config
 	ret = proxy_load_conf(&ph, opts.config_path);
@@ -339,6 +369,7 @@ int main(int argc, const char *argv[])
 	// Main dispatch loop
 	while (ph.status > PROXY_STATUS_DOWN)
 	{
+		proxy_log(&ph, LOG_LEVEL_DEBUG, "Starting a processing run...\n");
 		ret = proxy_process(&ph);
 		if (ret < 0)
 		{

commit 3f9701b50aefc15054f97a4b90db4398930c54da

Fix a bug when clients disconnect
Regression caused by 5796c3dd0efd3114e0e2ec28ddf13850952b8078

src/proxy.c
@@ -953,7 +953,7 @@ void * tcp_forwarder(void *ctx)
 	conn_close(fc->conn);
 
 	ret = send_tcp_close(fc->ph);
-	if (ret < 0)
+	if (ret < 0 && ret != -EPIPE)
 	{
 		// This is an error with this connection
 		proxy_log(fc->ph, LOG_LEVEL_WARN, "TCP communication error (%d). Disconnecting...\n", -ret);