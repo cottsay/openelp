cottsay/openelp

commit 8cc7d3b11869fe0724e6f59e4e6df71f424802bb

Re-format code to better match the Linux kernel
Also const-ify a bunch of const pointers, and re-arrange some struct
members to reduce padding.

src/digest.c
@@ -44,9 +44,6 @@
  * @brief Digest generation implementation
  */
 
-#include "digest.h"
-#include "md5.h"
-
 #include <errno.h>
 #include <stdio.h>
 
@@ -56,13 +53,16 @@
 #  include <arpa/inet.h>
 #endif
 
+#include "digest.h"
+#include "md5.h"
+
 /*!
  * @brief Converts a 8-bit value to a base 16 string
  *
  * @param[in] data Numeric value to convert
  * @param[out] result Resulting ASCII characters
  */
-static inline void digest_to_hex8(const uint8_t data, char result[2]);
+static void digest_to_hex8(uint8_t data, char result[2]);
 
 /*!
  * @brief Converts a 8-bit value to a base 16 string with uppercase letters
@@ -70,7 +70,7 @@ static inline void digest_to_hex8(const uint8_t data, char result[2]);
  * @param[in] data Numeric value to convert
  * @param[out] result Resulting ASCII characters
  */
-static inline void digest_to_hex8_u(const uint8_t data, char result[2]);
+static void digest_to_hex8_u(uint8_t data, char result[2]);
 
 /*!
  * @brief Converts a big-endian 32-bit value to a base 16 string
@@ -78,7 +78,7 @@ static inline void digest_to_hex8_u(const uint8_t data, char result[2]);
  * @param[in] data Numeric value to convert (big-endian)
  * @param[out] result Resulting ASCII characters
  */
-static inline void digest_to_hex32_be(const uint32_t data, char result[8]);
+static void digest_to_hex32_be(uint32_t data, char result[8]);
 
 /*!
  * @brief Converts a base 16 string to a 4-bit value
@@ -86,7 +86,7 @@ static inline void digest_to_hex32_be(const uint32_t data, char result[8]);
  * @param[in] data ASCII character to convert
  * @returns Resulting numeric value
  */
-static inline uint8_t hex4_to_digest(const char data);
+static uint8_t hex4_to_digest(char data);
 
 /*!
  * @brief Converts a base 16 string to an 8-bit value
@@ -94,9 +94,10 @@ static inline uint8_t hex4_to_digest(const char data);
  * @param[in] data ASCII characters to convert
  * @returns Resulting numeric value
  */
-static inline uint8_t hex8_to_digest(const char data[2]);
+static uint8_t hex8_to_digest(const char data[2]);
 
-void digest_get(const uint8_t *data, const unsigned int len, uint8_t result[DIGEST_LEN])
+void digest_get(const uint8_t *data, unsigned int len,
+		uint8_t result[DIGEST_LEN])
 {
 	MD5_CTX ctx;
 
@@ -107,97 +108,86 @@ void digest_get(const uint8_t *data, const unsigned int len, uint8_t result[DIGE
 	MD5_Final((unsigned char *)result, &ctx);
 }
 
-static inline void digest_to_hex8(const uint8_t data, char result[2])
+static void digest_to_hex8(uint8_t data, char result[2])
 {
-	static const char lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+	static const char lookup[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+	};
 
 	result[0] = lookup[data / 16];
 	result[1] = lookup[data % 16];
 }
 
-static inline void digest_to_hex8_u(const uint8_t data, char result[2])
+static void digest_to_hex8_u(uint8_t data, char result[2])
 {
-	static const char lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	static const char lookup[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+	};
 
 	result[0] = lookup[data / 16];
 	result[1] = lookup[data % 16];
 }
 
-void digest_to_hex32(const uint32_t data, char result[8])
+void digest_to_hex32(uint32_t data, char result[8])
 {
 	digest_to_hex32_be(htonl(data), result);
 }
 
-static inline void digest_to_hex32_be(const uint32_t data, char result[8])
+static void digest_to_hex32_be(uint32_t data, char result[8])
 {
-	const uint8_t *tgt = (const uint8_t *)&data;
-
-	digest_to_hex8(tgt[0], &result[0]);
-	digest_to_hex8(tgt[1], &result[2]);
-	digest_to_hex8(tgt[2], &result[4]);
-	digest_to_hex8(tgt[3], &result[6]);
+	digest_to_hex8(data & 0xff, &result[0]);
+	digest_to_hex8(data >> 0x8 & 0xff, &result[2]);
+	digest_to_hex8(data >> 0x10 & 0xff, &result[4]);
+	digest_to_hex8(data >> 0x18 & 0xff, &result[6]);
 }
 
-void digest_to_str(const uint8_t md5[DIGEST_LEN], char result[2 * DIGEST_LEN + 1])
+void digest_to_str(const uint8_t md5[DIGEST_LEN],
+		   char result[2 * DIGEST_LEN + 1])
 {
 	size_t i;
 
 	for (i = 0; i < DIGEST_LEN; i++, result += 2)
-	{
 		digest_to_hex8_u(md5[i], result);
-	}
 }
 
-static inline uint8_t hex4_to_digest(const char data)
+static uint8_t hex4_to_digest(char data)
 {
 	char base = '0';
 
-	if (data > '9')
-	{
-		if (data > 'F')
-		{
+	if (data > '9') {
+		if (data > 'F') {
 			if (data > 'f')
-			{
 				return 0;
-			}
-			else
-			{
-				base = 'a' - 10;
-			}
-		}
-		else
-		{
+			base = 'a' - 10;
+		} else {
 			base = 'A' - 10;
 		}
 	}
 
 	if (data < base)
-	{
 		return 0;
-	}
 
 	return data - base;
 }
 
-static inline uint8_t hex8_to_digest(const char data[2])
+static uint8_t hex8_to_digest(const char data[2])
 {
 	return (hex4_to_digest(data[0]) * 16) + hex4_to_digest(data[1]);
 }
 
 uint32_t hex32_to_digest(const char data[8])
 {
-	union
-	{
-		uint32_t value;
-		uint8_t parts[4];
-	} result = {
-		.parts = {
-			hex8_to_digest(&data[0]),
-			hex8_to_digest(&data[2]),
-			hex8_to_digest(&data[4]),
-			hex8_to_digest(&data[6]),
-		},
-	};
+	union {
+		uint32_t	value;
+		uint8_t		parts[4];
+	} result;
+
+	result.parts[0] = hex8_to_digest(&data[0]);
+	result.parts[1] = hex8_to_digest(&data[2]);
+	result.parts[2] = hex8_to_digest(&data[4]);
+	result.parts[3] = hex8_to_digest(&data[6]);
 
 	return ntohl(result.value);
 }

CMakeLists.txt
@@ -231,9 +231,9 @@ else()
   endif()
 endif()
 
-# Default to C11
+# Default to C90
 if(NOT CMAKE_C_STANDARD)
-  set(CMAKE_C_STANDARD 11)
+  set(CMAKE_C_STANDARD 90)
 endif()
 
 #

include/conf.h
@@ -44,12 +44,8 @@
  * @brief Internal API for configuration values
  */
 
-#ifndef _conf_h
-#define _conf_h
-
-#include "openelp/openelp.h"
-
-#include "log.h"
+#ifndef CONF_H_
+#define CONF_H_
 
 #include <stdint.h>
 #include <stdio.h>
@@ -57,6 +53,9 @@
 #ifndef _WIN32
 #  include <unistd.h>
 #endif
+
+#include "openelp/openelp.h"
+#include "log.h"
 
 /*!
  * @brief Frees data allocated by ::conf_init
@@ -79,10 +78,11 @@ int conf_init(struct proxy_conf *conf);
  *
  * @param[in] file Null-terminated string containing the path to the config
  * @param[in,out] conf Target configuration instance
- * @param[in,out] log Log handle for reporting logging events
+ * @param[in,out] log Handle for reporting logging events
  *
  * @returns 0 on success, negative ERRNO value on failure
  */
-int conf_parse_file(const char *file, struct proxy_conf *conf, struct log_handle *log);
+int conf_parse_file(const char *file, struct proxy_conf *conf,
+		    struct log_handle *log);
 
-#endif /* _conf_h */
+#endif /* CONF_H_ */

include/conn.h
@@ -44,8 +44,8 @@
  * @brief Internal API for network connections
  */
 
-#ifndef _conn_h
-#define _conn_h
+#ifndef CONN_H_
+#define CONN_H_
 
 #include <stdint.h>
 
@@ -56,13 +56,12 @@
 /*!
  * @brief Supported connection protocols
  */
-enum CONN_TYPE
-{
-	/// Transmission Control Protocol
+enum CONN_TYPE {
+	/*! Transmission Control Protocol */
 	CONN_TYPE_TCP,
 
-	/// User Datagram Protocol
-	CONN_TYPE_UDP,
+	/*! User Datagram Protocol */
+	CONN_TYPE_UDP
 };
 
 /*!
@@ -72,18 +71,17 @@ enum CONN_TYPE
  * should be initialized using the ::conn_init function, and subsequently
  * freed by ::conn_free when the network connection is no longer needed.
  */
-struct conn_handle
-{
-	/// Private data - used internally by conn functions
+struct conn_handle {
+	/*! Private data - used internally by conn functions */
 	void *priv;
 
-	/// Local network interface to bind to, or NULL for all
+	/*! Local network interface to bind to, or NULL for all */
 	const char *source_addr;
 
-	/// Local socket port to bind to, or NULL for any
+	/*! Local socket port to bind to, or NULL for any */
 	const char *source_port;
 
-	/// Protocol to use for this connection
+	/*! Protocol to use for this connection */
 	enum CONN_TYPE type;
 };
 
@@ -178,7 +176,8 @@ int conn_recv(struct conn_handle *conn, uint8_t *buff, size_t buff_len);
  *
  * @returns Number of bytes copied on success, negative ERRNO value on failure
  */
-int conn_recv_any(struct conn_handle *conn, uint8_t *buff, size_t buff_len, uint32_t *addr, uint16_t *port);
+int conn_recv_any(struct conn_handle *conn, uint8_t *buff, size_t buff_len,
+		  uint32_t *addr, uint16_t *port);
 
 /*!
  * @brief Send data to the connected client
@@ -202,7 +201,8 @@ int conn_send(struct conn_handle *conn, const uint8_t *buff, size_t buff_len);
  *
  * @returns 0 on success, negative ERRNO value on failure
  */
-int conn_send_to(struct conn_handle *conn, const uint8_t *buff, size_t buff_len, uint32_t addr, uint16_t port);
+int conn_send_to(struct conn_handle *conn, const uint8_t *buff,
+		 size_t buff_len, uint32_t addr, uint16_t port);
 
 /*!
  * @brief Stops socket operations but does not close the socket
@@ -219,4 +219,4 @@ void conn_shutdown(struct conn_handle *conn);
  */
 void conn_get_remote_addr(const struct conn_handle *conn, char dest[46]);
 
-#endif /* _conn_h */
+#endif /* CONN_H_ */

include/conn_wsa_errno.h
@@ -44,14 +44,14 @@
  * @brief Internal API for retrieving WSA errors
  */
 
-#ifndef _conn_wsa_errno_h
-#define _conn_wsa_errno_h
+#ifndef CONN_WSA_ERRNO_H_
+#define CONN_WSA_ERRNO_H_
 
 /*!
  * @brief Retrieves the last WSA error
  *
  * @returns ERRNO value, or WSA error if conversion failed
  */
-int conn_wsa_errno();
+int conn_wsa_errno(void);
 
-#endif /* _conn_wsa_errno_h */
+#endif /* CONN_WSA_ERRNO_H_ */

include/digest.h
@@ -44,8 +44,8 @@
  * @brief Internal API for digest generation
  */
 
-#ifndef _digest_h
-#define _digest_h
+#ifndef DIGEST_H_
+#define DIGEST_H_
 
 #include <stdint.h>
 
@@ -53,17 +53,18 @@
 #  include <unistd.h>
 #endif
 
-/// Length in bytes of all digests
+/*! Length in bytes of all digests */
 #define DIGEST_LEN 16
 
 /*!
  * @brief Calculates the digest of the given data
  *
- * @param[in] data Data whose digest is calculated
+ * @param[in] data The data whose digest is calculated
  * @param[in] len Number of bytes at the location indicated by data
  * @param[out] result Resulting digest value
  */
-void digest_get(const uint8_t *data, const unsigned int len, uint8_t result[DIGEST_LEN]);
+void digest_get(const uint8_t *data, unsigned int len,
+		uint8_t result[DIGEST_LEN]);
 
 /*!
  * @brief Converts a 32-bit value to a base 16 string
@@ -71,7 +72,7 @@ void digest_get(const uint8_t *data, const unsigned int len, uint8_t result[DIGE
  * @param[in] data Numeric value to convert
  * @param[out] result Resulting ASCII characters
  */
-void digest_to_hex32(const uint32_t data, char result[8]);
+void digest_to_hex32(uint32_t data, char result[8]);
 
 /*!
  * @brief Converts the given digest to a null terminated string
@@ -79,7 +80,8 @@ void digest_to_hex32(const uint32_t data, char result[8]);
  * @param[in] md5 Input digest value
  * @param[out] result Resulting string representation of the given digest
  */
-void digest_to_str(const uint8_t md5[DIGEST_LEN], char result[2 * DIGEST_LEN + 1]);
+void digest_to_str(const uint8_t md5[DIGEST_LEN],
+		   char result[2 * DIGEST_LEN + 1]);
 
 /*!
  * @brief Converts a base 16 string to a 32-bit value
@@ -89,4 +91,4 @@ void digest_to_str(const uint8_t md5[DIGEST_LEN], char result[2 * DIGEST_LEN + 1
  */
 uint32_t hex32_to_digest(const char data[8]);
 
-#endif /* _digest_h */
+#endif /* DIGEST_H_ */

include/log.h
@@ -44,8 +44,8 @@
  * @brief Internal API for message logging
  */
 
-#ifndef _log_h
-#define _log_h
+#ifndef LOG_H_
+#define LOG_H_
 
 #include <stdarg.h>
 #include <stdint.h>
@@ -59,16 +59,15 @@
  * should be initialized using the ::log_init function, and subsequently
  * freed by ::log_free when the logging infrastructure is no longer needed.
  */
-struct log_handle
-{
-	/// Severigy threshold for reporting log messages
+struct log_handle {
+	/*! Private data - used internally by log functions */
+	void *priv;
+
+	/*! Severigy threshold for reporting log messages */
 	uint32_t level;
 
-	/// Logging facility used to report logging event messages
+	/*! Logging facility used to report logging event messages */
 	uint32_t medium;
-
-	/// Private data - used internally by log functions
-	void *priv;
 };
 
 /*!
@@ -108,7 +107,7 @@ int log_init(struct log_handle *log);
  *
  * @returns Pointer to a null-terminated static const string
  */
-const char * log_medium_to_str(enum LOG_MEDIUM medium);
+const char *log_medium_to_str(enum LOG_MEDIUM medium);
 
 /*!
  * @brief Opens the log infrastructure by switching to ::LOG_MEDIUM_STDOUT
@@ -127,7 +126,8 @@ int log_open(struct log_handle *log);
  * @param[in] fmt String format of message
  * @param[in] ... Arguments for format specification
  */
-void log_printf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, ...);
+void log_printf(struct log_handle *log, enum LOG_LEVEL lvl,
+		const char *fmt, ...);
 
 /*!
  * @brief Changes the target logging medium
@@ -138,7 +138,8 @@ void log_printf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, ...
  *
  * @returns 0 on success, negative ERRNO value on failure
  */
-int log_select_medium(struct log_handle *log, const enum LOG_MEDIUM medium, const char *target);
+int log_select_medium(struct log_handle *log, enum LOG_MEDIUM medium,
+		      const char *target);
 
 /*!
  * @brief Logs the given message to the current logging facility
@@ -148,6 +149,7 @@ int log_select_medium(struct log_handle *log, const enum LOG_MEDIUM medium, cons
  * @param[in] fmt String format of message
  * @param[in] args Arguments for format specification
  */
-void log_vprintf(struct log_handle *log, enum LOG_LEVEL lvl, const char *fmt, va_list args);
+void log_vprintf(struct log_handle *log, enum LOG_LEVEL lvl,
+		 const char *fmt, va_list args);
 
-#endif /* _log_h */
+#endif /* LOG_H_ */

include/log_eventlog.h
@@ -44,19 +44,22 @@
  * @brief Defenitions and prototypes for interfacing with the Windows Event Log
  */
 
-#ifndef _log_eventlog_h
-#define _log_eventlog_h
+#ifndef LOG_EVENTLOG_H_
+#define LOG_EVENTLOG_H_
 
 #ifdef HAVE_EVENTLOG
 #  include <windows.h>
 #  include <winbase.h>
 #  include "log_eventlog_messages.h"
-#  define EVENTLOG_ERRNO -(long)GetLastError()
+#  define EVENTLOG_ERRNO (-(int64_t)GetLastError())
 typedef HANDLE EVENTLOG_HANDLE;
 #else
-#  define DeregisterEventSource(...)
-#  define RegisterEventSource(...) NULL
-#  define ReportEvent(handle, type, cat, eventId, userSid, numStrings, dataSize, strings, rawData) (void)handle, (void)type, (void)cat, (void)eventId, (void)userSid, (void)numStrings, (void)dataSize, (void)strings, (void)rawData
+#  define DeregisterEventSource(hEventLog)
+#  define RegisterEventSource(lpUNCServerName, lpSourceName) NULL
+#  define ReportEvent(handle, type, cat, eventId, userSid, numStrings, \
+	dataSize, strings, rawData) ((void)handle, (void)type, (void)cat, \
+	(void)eventId, (void)userSid, (void)numStrings, (void)dataSize, \
+	(void)strings, (void)rawData)
 #  define EVENTLOG_ERRNO -ENOTSUP
 #  define EVENTLOG_ERROR_TYPE 0
 #  define EVENTLOG_WARNING_TYPE 0
@@ -66,7 +69,7 @@ typedef HANDLE EVENTLOG_HANDLE;
 #  define LOG_IDENT_WARN 0
 #  define LOG_IDENT_INFO 0
 #  define LOG_IDENT_DEBUG 0
-typedef void * EVENTLOG_HANDLE;
+typedef void *EVENTLOG_HANDLE;
 #endif
 
-#endif /* _log_eventlog_h */
+#endif /* LOG_EVENTLOG_H_ */

commit 0bda099cd40386696375fb8f21c7850324e826fc

Fix digest regression on big endian systems
Regression caused by 8cc7d3b11869fe0724e6f59e4e6df71f424802bb

src/digest.c
@@ -73,14 +73,6 @@ static void digest_to_hex8(uint8_t data, char result[2]);
 static void digest_to_hex8_u(uint8_t data, char result[2]);
 
 /*!
- * @brief Converts a big-endian 32-bit value to a base 16 string
- *
- * @param[in] data Numeric value to convert (big-endian)
- * @param[out] result Resulting ASCII characters
- */
-static void digest_to_hex32_be(uint32_t data, char result[8]);
-
-/*!
  * @brief Converts a base 16 string to a 4-bit value
  *
  * @param[in] data ASCII character to convert
@@ -132,15 +124,10 @@ static void digest_to_hex8_u(uint8_t data, char result[2])
 
 void digest_to_hex32(uint32_t data, char result[8])
 {
-	digest_to_hex32_be(htonl(data), result);
-}
-
-static void digest_to_hex32_be(uint32_t data, char result[8])
-{
-	digest_to_hex8(data & 0xff, &result[0]);
-	digest_to_hex8(data >> 0x8 & 0xff, &result[2]);
-	digest_to_hex8(data >> 0x10 & 0xff, &result[4]);
-	digest_to_hex8(data >> 0x18 & 0xff, &result[6]);
+	digest_to_hex8(data >> 0x18 & 0xff, &result[0]);
+	digest_to_hex8(data >> 0x10 & 0xff, &result[2]);
+	digest_to_hex8(data >> 0x8 & 0xff, &result[4]);
+	digest_to_hex8(data & 0xff, &result[6]);
 }
 
 void digest_to_str(const uint8_t md5[DIGEST_LEN],